eb7afd45bbfe6c981aaa1bce2258f79a10bd66c8
KaterinaKourti <katherinekourti@gmail.com>
2022-03-13 19:15:11 +0200

    mapper for Q3

--- app/mapper/main.py
+++ app/mapper/main.py
@@ -1,14 +1,17 @@
 import argparse
+import datetime
 import haversine as hs
 import io
 from json import loads,dumps
 from minio import Minio
 import pandas as pd
+from termcolor import colored
+
 from utils.data_constants import *
 from utils.minio_utils import create_bucket
  
 parser = argparse.ArgumentParser()
-parser.add_argument("-query", default = 2, type = int)
+parser.add_argument("-query", default = 3, type = int)
 args = parser.parse_args()
 
 
@@ -98,6 +101,30 @@ def shuffle_trip_stats(batch):
     return [effective_trips, ineffective_trips]
 
 
+def optimal_trips(row):
+    date = row['pickup_datetime']
+    date_formatted = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    week_day = date_formatted.isoweekday()
+    # time = datetime.time(date_formatted.hour,date_formatted.minute, date_formatted.second)
+    time = date_formatted.time()
+    lower_morning_rush = datetime.time(7, 0, 0)
+    upper_morning_rush = datetime.time(10, 0, 0)
+    lower_noon_rush = datetime.time(15, 0, 0)
+    upper_noon_rush = datetime.time(19, 0, 0)
+
+
+    # print(colored(f"Weekday: {week_day}",'blue'))
+    # print(colored(f"Time: {time}",'yellow'))
+    # print(colored(f"Lower morning rush: {lower_morning_rush}",'magenta'))
+
+    if week_day in [1,5] and (lower_morning_rush <= time <= upper_morning_rush or lower_noon_rush <= time <= upper_noon_rush):
+        quarter = find_quarter(row)
+        # print(colored(quarter,'magenta'))
+        return quarter
+        
+
+
+
 def mapper(client,bucket,index,batch,query):
 
     if query == 1:
@@ -114,11 +141,19 @@ def mapper(client,bucket,index,batch,query):
         
     
     else:
-        pass
+        
         # Query 3 Logic
+        # optimals = list(map(lambda row: optimal_trips(row) , batch))
+        # optimals_filtered = list(filter(lambda x: x is not None, optimals))
+        optimals = [optimal_trips(x) for x in batch if optimal_trips(x) is not None]
+        # print(optimals)
+        mapped_data = shuffle_quarters(optimals)
+        data = dumps(mapped_data)
+        # print(colored(mapped_data,'cyan'))
 
-
-
+        response = client.put_object(bucket, f"Q3_mapper_results_batch_{index}", io.BytesIO(bytes(data,'ascii')), len(bytes(data,'ascii')), content_type="application/json")
+        
+    
 
 
 def main(args):
